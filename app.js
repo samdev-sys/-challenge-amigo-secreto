d576741e27cf71fbd9803ded812a81fec9d0c40f
samdev-sys <sv4432@gmail.com>
2025-02-12 22:54:26 -0500

    inicio challennge amigo secreto

---
+++ app.js
@@ -0,0 +1,18 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let amigos=[];
+
+
+function ingresarAmigo(){
+    let ingresoAmigo = (document.getElementById('amigo').value);
+    console.log(ingresoAmigo);
+
+    if(ingresoAmigo==''){
+        alert('Por favor, inserte un nombre.');
+    }else{
+        amigos.push();
+        console.log(amigos);
+    }
+}
+
